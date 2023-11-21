c5df038d7176ba5ead9310ac4a998a5c6368cfab
liveinsidee <150421303+liveinsidee@users.noreply.github.com>
2023-11-21 19:48:25 +0600

    Add files via upload

---
+++ дз 3/app.js
@@ -0,0 +1,34 @@
+// дз-1
+// var array = ['li', 'div', 'li', 'p', 'h1', 'p', 'h1', 'div', 'div', 'li', 'h1', 'p', 'h1', 'ol', 'or']
+// var tagCounts = {};
+// array.forEach(function (tag) {
+//     tagCounts[tag] = (tagCounts[tag] || 0) + 1;
+// })
+// console.log(tagCounts)
+
+
+// дз-2
+// var starbucks = {
+//     coffee:{
+//         espresso: "Strong black coffee",
+//         latte: "Coffee with milk",
+//     },
+//     tea:{
+//         greenTea: "Зеленый чай",
+//         earlGrey: "Черный чай",
+//     },
+// };
+// function order(type, name) {
+//     var category = starbucks[type];
+//     if (category) {
+//         var item = category[name];
+//         if (item) {
+//             console.log(`Заказ принят ${name}: ${item}`);
+//         } else {
+//             console.error(`Error: ${name} ${type}`);
+//         }
+//     } else {
+//         console.error(`Error: ${type}`);
+//     }
+// }
+//
